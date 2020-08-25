604b8641ec49e8a685c14dd2201419e4a69ed987
Zexd3 <khairuleidlan14@gmail.com>
2020-08-25 10:40:30 +0800

    modal view

---
+++ resources/views/client/view.blade.php
@@ -0,0 +1,57 @@
+<div class="col-md-12">
+    <section class="panel panel-featured panel-featured-info">
+        <header class="panel-heading" style="background: -webkit-linear-gradient(top, #b0c4de 43%,#ffffff 100%);">
+            <h6 class="panel-title"><font color="#000000" size="3"><b>Maklumat Permohonan</b></font></h6>
+        </header>
+        <div class="panel-body">
+            <div class="form-group">
+                <div class="row">
+                    <label class="col-sm-2 control-label">Nama Saintifik :</label>
+                    <div class="col-sm-6">
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <label class="col-sm-2 control-label">Nama Saintifik :</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <label class="col-sm-2 control-label">Nama Saintifik :</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <label class="col-sm-2 control-label">Nama Saintifik :</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <label class="col-sm-2 control-label">Nama Saintifik :</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <label class="col-sm-2 control-label">Nama Saintifik :</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <label class="col-sm-2 control-label">Nama Saintifik :</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <label class="col-sm-2 control-label">Nama Saintifik :</label>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <label class="col-sm-2 control-label">Nama Saintifik :</label>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
