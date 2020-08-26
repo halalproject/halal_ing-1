45df6b36d1bd4ede9d9af99de1d35c17004b85cb
Zexd3 <khairuleidlan14@gmail.com>
2020-08-26 12:57:20 +0800

    style of dashboard

--- resources/views/components/page.blade.php
+++ resources/views/components/page.blade.php
@@ -2,8 +2,8 @@
 
 @section('page')
 <section class="content-body">
-            <header class="page-header visible-lg" style="background: rgb(244,164,96);background: linear-gradient(90deg, rgba(244,164,96,1) 0%, rgba(222,184,135,1) 0%, rgba(210,180,140,0.7651261188068977) 100%);">
-                <h2 style="color:#000">Sistem MyHalal Ingredient</h2>
+            <header class="page-header visible-lg" style="background: #34495e;background: #1C2B36">
+                <h2>Sistem MyHalal Ingredient</h2>
             
                 <div class="right-wrapper pull-right">
                     <ol class="breadcrumbs">
@@ -19,17 +19,17 @@
                     <a class="sidebar-right-toggle" data-open="sidebar-right"><i class="fa fa-chevron-left"></i></a>
                 </div>
             </header>
-            <header class="page-header" style="background: rgb(244,164,96);background: linear-gradient(90deg, rgba(244,164,96,1) 0%, rgba(222,184,135,1) 0%, rgba(210,180,140,0.7651261188068977) 100%);">
-                <h2 style="color:#000">Sistem MyHalal Ingredient</h2>
+            <header class="page-header" style="background: #34495e;background: #1C2B36">
+                <h2>Sistem MyHalal Ingredient</h2>
             
                 <div class="right-wrapper pull-right">
                     <ol class="breadcrumbs">
-                        <li style="color:#000">
+                        <li>
                             <a href="/">
-                                <i class="fa fa-home" style="color:#000"></i>
+                                <i class="fa fa-home"></i>
                             </a>
                         </li>
-                        <li style="color:#000"><span style="color:#000"><b>MyHalal Ingredient @if(!empty(\Request::route()->getname())) / {{ \Request::route()->getname() }} @endif&nbsp;</b></span></li>
+                        <li><span><b>MyHalal Ingredient @if(!empty(\Request::route()->getname())) / {{ \Request::route()->getname() }} @endif&nbsp;</b></span></li>
                         
                     </ol>
                 </div>
