97155f56a0f9fa40c6e8ab82d0cedaf8d3f01312
Zexd3 <khairuleidlan14@gmail.com>
2020-08-26 12:00:46 +0800

    admin chnages

--- resources/views/admin/ramuan.blade.php
+++ resources/views/admin/ramuan.blade.php
@@ -15,7 +15,7 @@
                 <!--<a href="#" class="fa fa-caret-down"></a>
                 <a href="#" class="fa fa-times"></a>-->
                 </div>
-                <h6 class="panel-title"><font color="#000000"><b>SENARAI PELANGGAN</b></font></h6> 
+                <h6 class="panel-title"><font color="#000000"><b>SENARAI RAMUAN</b></font></h6> 
             </header>
 			</div>
             </div>            
@@ -54,32 +54,39 @@
               <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
                 <thead>
                 <tr style="background: -webkit-linear-gradient(top, #b0c4de 43%,#ffffff 100%);">
-                  <th width="5%"><font color="#000000"><div align="left">#</div></font></th>
-                  <th width="20%"><font color="#000000"><div align="left">Nama Syarikat</div></font></th>
-                  <th width="10%"><font color="#000000"><div align="left">Kategori</div></font></th>
-                  <th width="10%"><font color="#000000"><div align="left">No. Syarikat</font></th>
-                  <th width="10%"><font color="#000000"><div align="left">H/P</div></font></th>
-                  <th width="10%"><font color="#000000"><div align="left">Emel</div></font></th>
-                  <th width="20%"><font color="#000000"><div align="left">Alamat</div></font></th>
-                  <th width="20%"><font color="#000000"><div align="left">Senarai Ramuan</div></font></th>
+                  <th width="5%"><font color="#000000"><div align="left">Bil.</div></font></th>
+                  <th width="17%"><font color="#000000"><div align="left">No. Permohonan</div></font></th>
+                  <th width="20%"><font color="#000000"><div align="left">Nama Ramuan</div></font></th>
+                  <th width="15%"><font color="#000000"><div align="left">Kategori</font></th>
+                  <th width="13%"><font color="#000000"><div align="left">Tarikh Permohonan</font></th>
+                  <th width="13%"><font color="#000000"><div align="left">Tempoh Tamat Sijil</font></th>
+                  <th width="15%"><font color="#000000"><div align="left">Tindakan</div></font></th>
                 </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td>1</td>
-                        <td><a href="/admin/syarikat/view/12" data-toggle="modal" data-target="#myModal" title="Nama Syarikat" class="fa" data-backdrop="static">
-                        TESTING HALAL JAKIM</td>
-                        <td>Pembekal</td>
-                        <td>000000</td>
-                        <td>0137057666</td>
-                        <td>ICTHALAL@ISLAM.GOV.MY</td>
-                        <td>qwerty </td>
-                        <td>
-                            <a href="/admin/syarikat/ramuan/12">
-                                <span class="btn btn-primary">
-                                    <i class="fa fa-list"></i>
-                                </span>
+                        <td>HQ0012411123</td>
+                        <td>Ayam</td>
+                        <td>Semulajadi</td>
+                        <td>20/8/2020</td>
+                        <td>2 Tahun</td>
+                        <td align="center">
+                            <a href="/client/proses/view/12" data-toggle="modal" data-target="#myModal" title="Maklumat Ramuan" class="fa" data-backdrop="static">
+                                <button type="button" class="btn btn-sm btn-info">
+                                    <i class="fa fa-file-text fa-lg" style="color: #FFFFFF;"></i>
+                                </button>
                             </a>
+                            <a href="/client/proses/edit/12" data-toggle="modal" data-target="#myModal" title="Kemaskini Ramuan" class="fa" data-backdrop="static">
+                                <button type="button" class="btn btn-sm btn-warning">
+                                    <i class="fa fa-pencil-square-o fa-lg" style="color: #FFFFFF;"></i>
+                                </button>
+                            </a>
+                            <button type="button" class="btn btn-sm btn-danger" onclick="do_hapus()">
+                                <span style="cursor:pointer;color:red" title="Buang Ramuan">
+                                    <i class="fa fa-trash-o fa-lg" style="color: #FFFFFF;"></i>
+                                </span>
+                            </button>
                         </td>
                     </tr>
                 </tbody>
