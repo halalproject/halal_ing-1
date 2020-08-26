286a48aaedc0d460310fcd105bfa6d1c524d6fc0
hidayu mutalib <hidayumutalib@gmail.com>
2020-08-26 09:41:41 +0800

    admin upload

--- resources/views/admin/modalStaff.blade.php
+++ resources/views/admin/modalStaff.blade.php
@@ -1,4 +1,8 @@
 <script>
+    $("button[type='button']").click(function() {
+        $("input[id='my_file']").click();
+    });
+
     function do_close() {
         location.reload();
     }
@@ -9,7 +13,7 @@ $bahan_rs = $rs->sumber_bahan_id ?? '';
 $negara_rs = $rs->negara_pengilang_id ?? '';
 $negeri_rs = $rs->negeri_pembekal_id ?? '';
 @endphp
-<div class="col-md-12">
+<div class="col-md-8" style="top:300px;left: 50%;transform: translate(-50%, -50%);">
 <form name="halal" id="create" method="post" action="" enctype="multipart/form-data" autocomplete="off">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <section class="panel panel-featured panel-featured-info">
@@ -24,9 +28,18 @@ $negeri_rs = $rs->negeri_pembekal_id ?? '';
 
             <div>
             <!-- gambar -->
-            
-            </div>
+                <!-- <table>
+                    <img style="margin-left:250px;" src="{{ asset('images/person.jpg') }}" width="200" height="200" />
+                    <tr><td><input type="file" name="image" style="padding-left:260px;"></td></tr>
+                </table> -->
 
+                <body>
+                    <input type="image" src="{{ asset('images/person.jpg') }}" width="200" height="200" style="margin-left:250px;"/>
+                    <input type="file" id="my_file" style="padding-left:260px;display: none;" /><br>
+                    <button type="button" class="btn btn-info" style="margin-left:263px;" id="upload"><i class="fa fa-upload" aria-hidden="true"></i> Muat Naik Gambar</button>
+                </body>
+            </div>
+            <br>
             <div class="form-group">
                 <div class="row">
                     <label class="col-sm-2 control-label"><font color="#FF0000">*</font> Nama :</label>
