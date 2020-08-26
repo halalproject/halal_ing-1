b9b7f4885c597056009e4fedcda4330288be01d7
Zexd3 <khairuleidlan14@gmail.com>
2020-08-26 16:46:20 +0800

    calender dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -39,54 +39,34 @@
                         </section>
                     </div>
                     
-                    <div class="col-md-12 col-lg-4 col-xl-4">
-                        <section class="panel panel-featured-left panel-featured-secondary">
-                            <div class="panel-body">
-                                <div class="widget-summary">
-                                    <div class="widget-summary-col widget-summary-col-icon">
-                                        <div class="summary-icon bg-secondary">
-                                            <i class="fa fa-list"></i>
-                                        </div>
-                                    </div>
-                                    <div class="widget-summary-col">
-                                        <div class="summary">
-                                            <h4 class="title">Senarai Ramuan</h4>
-                                            <div class="info">
-                                                <strong class="amount"></strong>
-                                            </div>
-                                        </div>
-                                        <div class="summary-footer">
-                                            <a href="" class="text-muted text-uppercase">Paparan Maklumat</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </section>
+                    <div class="col-md-12 col-lg-2 col-xl-2">
+                        
                     </div>
                     
-                    <div class="col-md-12 col-lg-4 col-xl-4">
-                        <section class="panel panel-featured-left panel-featured-tertiary">
-                            <div class="panel-body">
-                                <div class="widget-summary">
-                                    <div class="widget-summary-col widget-summary-col-icon">
-                                        <div class="summary-icon bg-tertiary">
-                                            <i class="fa fa-folder"></i>
-                                        </div>
-                                    </div>
-                                    <div class="widget-summary-col">
-                                        <div class="summary">
-                                            <h4 class="title">Ramuan Yang Dihapuskan</h4>
-                                            <div class="info">
-                                                <strong class="amount"></strong>
+                    <div class="col-md-12 col-lg-6 col-xl-6">
+                        <div class="row">
+                            <section class="panel" style="cursor: pointer" onclick="do_berita()">
+                                <div class="panel-body">
+                                    <div class="widget-summary">
+                                        <div class="widget-summary-col">
+                                            <div class="summary">
+                                                <h4 class="title">Berita</h4>
+                                                <div class="info">
+                                                    <i class="">Berita</i>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div class="summary-footer">
-                                            <a href="" class="text-muted text-uppercase">Papar Maklumat</a>
+                                            <div class="summary-footer" style="text-align: left">
+                                                <i class="fa fa-clock-o"></i> 2019-10-12 &nbsp; <i class="fa fa-play-circle-o"></i> Lihat 1
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </section>
+                            </section>
+                        </div>
+                        <div class="row">
+                            
+                        </div>
+                        
                     </div>
                 </div>
             </div>
