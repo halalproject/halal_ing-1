c384d8d9fc8f62346cac05f348a3a75b3fadc6fc
ftninomr <fatninomar21@gmail.com>
2020-08-28 12:15:46 +0800

    ADMIN

--- resources/views/client/ramuanhapus.blade.php
+++ resources/views/client/ramuanhapus.blade.php
@@ -86,7 +86,7 @@
                             </a>
                             <button type="button" class="btn btn-sm btn-danger" onclick="do_hapus()">
                                 <span style="cursor:pointer;color:red" title="Buang Ramuan">
-                                    <i class="fa fa-trash fa-lg" style="color: #FFFFFF;"></i>
+                                    <i class="fa fa-undo fa-lg" style="color: #FFFFFF;"></i>
                                 </span>
                             </button>
                         </td>
@@ -114,7 +114,7 @@
                             </a>
                             <button type="button" class="btn btn-sm btn-danger" onclick="do_hapus()">
                                 <span style="cursor:pointer;color:red" title="Buang Ramuan">
-                                    <i class="fa fa-trash fa-lg" style="color: #FFFFFF;"></i>
+                                    <i class="fa fa-undo fa-lg" style="color: #FFFFFF;"></i>
                                 </span>
                             </button>
                         </td>
@@ -142,7 +142,7 @@
                             </a>
                             <button type="button" class="btn btn-sm btn-danger" onclick="do_hapus()">
                                 <span style="cursor:pointer;color:red" title="Buang Ramuan">
-                                    <i class="fa fa-trash fa-lg" style="color: #FFFFFF;"></i>
+                                    <i class="fa fa-undo fa-lg" style="color: #FFFFFF;"></i>
                                 </span>
                             </button>
                         </td>
