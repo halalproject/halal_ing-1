0720c7260d6cbbaae6827eb4603bcf5af162c92a
Zexd3 <khairuleidlan14@gmail.com>
2020-09-01 16:43:27 +0800

    profile modal

--- resources/views/client/modal.blade.php
+++ resources/views/client/modal.blade.php
@@ -320,10 +320,12 @@ if(!empty($id)){
                                     <div class="col-sm-8 control-label">
                                         <input type="text" name="kilang_alamat_2" id="kilang_alamat_2" placeholder="Alamat 2" class="form-control" value="{{$rs->alamat_pengilang_2??''}}">
                                     </div>
+
                                     <label class="col-sm-3 control-label"></label>
                                     <div class="col-sm-8 control-label">
                                         <input type="text" name="kilang_bandar" id="kilang_bandar" placeholder="Bandar" class="form-control" value="{{$rs->alamat_pengilang_3??''}}">
                                     </div>
+                                    
                                     <label class="col-sm-3 control-label"></label>
                                     <div class="col-sm-2 control-label">
                                         <input type="text" name="kilang_poskod" id="kilang_poskod" placeholder="Poskod" class="form-control" value="{{$rs->poskod_pengilang??''}}">
--- resources/views/client/profile.blade.php
+++ resources/views/client/profile.blade.php
@@ -14,7 +14,59 @@ function do_close()
             <div class="col-md-12">
                 <input type="hidden" name="id" id="id" class="form-control" value="">
                 <div class="form-group">
-                    Profile
+                    <div class="row">
+                        <label class="col-sm-2 control-label"><font color="#FF0000">*</font> Nama Syarikat :</label>
+                        <div class="col-sm-5">
+                            <input type="text" name="company_name" id="company_name" class="form-control" value="">
+                        </div>
+                        <label class="col-sm-3 control-label"><font color="#FF0000">*</font> No Pendaftaran Syarikat :</label>
+                        <div class="col-sm-2">
+                            <input type="text" name="company_reg_code" id="company_reg_code" class="form-control" value="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="row">
+                        <label class="col-sm-2 control-label"><font color="#FF0000">*</font> Alamat Syarikat :</label>
+                        <div class="col-sm-10">
+                            <input type="text" placeholder="Alamat 1" name="company_address_1" id="company_address_1" class="form-control" value="">
+                        </div>
+                        <label class="col-sm-2 control-label"></label>
+                        <div class="col-sm-10">
+                            <input type="text" placeholder="Alamat 2" name="company_address_2" id="company_address_2" class="form-control" value="">
+                        </div>
+                        <label class="col-sm-2 control-label"></label>
+                        <div class="col-sm-10">
+                            <input type="text" placeholder="Alamat 3" name="company_address_3" id="company_address_3" class="form-control" value="">
+                        </div>
+                        <label class="col-sm-2 control-label"></label>
+                        <div class="col-sm-2">
+                            <input type="text" placeholder="Poskod" name="company_poscode" id="company_poscode" class="form-control" value="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="row">
+                        <label class="col-sm-2 control-label"><font color="#FF0000">*</font> Nama Ramuan :</label>
+                        <div class="col-sm-8">
+                            <input type="text" name="ramuan" id="ramuan" class="form-control" value="">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="row">
+                        <label class="col-sm-2 control-label"><font color="#FF0000">*</font> Web :</label>
+                        <div class="col-sm-4">
+                            <input type="text" name="ramuan" id="ramuan" class="form-control" value="">
+                        </div>
+                        <label class="col-sm-2 control-label"><font color="#FF0000">*</font> Emel :</label>
+                        <div class="col-sm-4">
+                            <input type="text" name="ramuan" id="ramuan" class="form-control" value="">
+                        </div>
+                    </div>
                 </div>
                 
                 <div class="form-group">
