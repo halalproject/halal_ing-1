63d13fbc65ac5dabe89aa139c79e0da70f3783f1
hidayu mutalib <hidayumutalib@gmail.com>
2020-10-06 11:21:54 +0800

    admin

--- resources/views/client/view.blade.php
+++ resources/views/client/view.blade.php
@@ -1,7 +1,24 @@
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.22/pdfmake.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js"></script>
 <script>
 function do_close()
 {
     location.reload();
+}
+
+function Export() {
+    html2canvas(document.getElementById('maklumatPermohonan'), {
+        onrendered: function (canvas) {
+            var data = canvas.toDataURL();
+            var docDefinition = {
+                content: [{
+                    image: data,
+                    width: 500
+                }]
+            };
+            pdfMake.createPdf(docDefinition).download("Maklumat_Permohonan.pdf");
+        }
+    });
 }
 </script>
 
@@ -26,89 +43,92 @@ if((!empty($id)) && ($upload != '')){
                 @if($rs->status == 1 && !empty($rs->tarikh_buka)) [Sedang Diproses] @elseif($rs->status == 6) [Tolak] @elseif($rs->is_delete == 1) [Hapus] @else @endif
                 </font>
 
-                <button type="button" class="btn btn-md btn-success printButton" style="float: right; background-color:#252396;" onclick="window.print()" id="cetak">
+                <button type="button" class="btn btn-md btn-success printButton" style="float: right; background-color:#252396;" onclick="Export()" id="btnExport" value="Export">
                     <i class="fa fa-print"></i> Cetak
                 </button>
             </h2>
         </header>
-        <div class="panel-body ">
+        <div class="panel-body" id="maklumatPermohonan">
             <div class="col-md-12">
                 <input type="hidden" name="id" id="id" class="form-control" value="{{$rs->id}}">
                 <div class="form-group">
                     <div class="row">
-                        <label class="col-sm-3 control-label"><b>No Permohonan :</b></label>
+                        <label class="col-sm-4 control-label"><b>No Permohonan :</b></label>
                         <div class="col-sm-5">{{$rs->ing_kod}}</div>
-
-                        <label class="col-sm-2 control-label"><b>Tarikh Permohonan :</b></label>
-                        <div class="col-sm-2">{{ date('d/m/Y',strtotime($rs->create_dt)) }}</div>
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="row">
-                        <label class="col-sm-3 control-label"><b>Nama Ramuan :</b></label>
+                        <label class="col-sm-4 control-label"><b>Tarikh Permohonan :</b></label>
+                        <div class="col-sm-5">{{ date('d/m/Y',strtotime($rs->create_dt)) }}</div>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="row">
+                        <label class="col-sm-4 control-label"><b>Nama Ramuan :</b></label>
                         <div class="col-sm-5">{{$rs->nama_ramuan}}</div>
                     </div>
                 </div>
 
                 <div class="form-group">
                     <div class="row">
-                        <label class="col-sm-3 control-label"><b>Nama Saintifik :</b></label>
+                        <label class="col-sm-4 control-label"><b>Nama Saintifik :</b></label>
                         <div class="col-sm-5">{{$rs->nama_saintifik}}</div>
                     </div>
                 </div>
 
                 <div class="form-group">
                     <div class="row">
-                        <label class="col-sm-3 control-label" for="profileLastName"><b>Sumber Bahan :</b></label>
+                        <label class="col-sm-4 control-label" for="profileLastName"><b>Sumber Bahan :</b></label>
                         <div class="col-sm-4">{{ optional($rs->sumber)->nama }}</div>
                     </div>
                 </div> 
 
                 <div class="form-group">
                     <div class="row">
-                        <label class="col-sm-3 control-label" for="profileLastName"><b>Negara Asal Pengilang/Pengeluar: </b></label>
+                        <label class="col-sm-4 control-label" for="profileLastName"><b>Negara Asal Pengilang/Pengeluar: </b></label>
                         <div class="col-sm-4">{{ optional($rs->negara)->nama }}</div>
                     </div>
                 </div>
                 
                 <div class="form-group">
                     <div class="row">
-                        <label class="col-sm-3 control-label"><b>Nama Pengilang/Pengeluar : </b></label>
+                        <label class="col-sm-4 control-label"><b>Nama Pengilang/Pengeluar : </b></label>
                         <div class="col-sm-5">{{$rs->nama_pengilang}}</div>
                     </div>
                 </div>
                 
                 <div class="form-group">
                     <div class="row">
-                        <label class="col-sm-3 control-label"><b>Alamat Pengilang/Pengeluar : </b></label>
+                        <label class="col-sm-4 control-label"><b>Alamat Pengilang/Pengeluar : </b></label>
                         <div class="col-sm-5">{{$rs->alamat_pengilang_1}} {{$rs->alamat_pengilang_2}} {{$rs->alamat_pengilang_3}} {{$rs->poskod_pengilang}}</div>
                     </div>
                 </div>
 
                 <div class="form-group">
                     <div class="row">
-                        <label class="col-sm-3 control-label" for="profileLastName"><b>Negeri Asal Pembekal: </b></label>
+                        <label class="col-sm-4 control-label" for="profileLastName"><b>Negeri Asal Pembekal: </b></label>
                         <div class="col-sm-4">{{ optional($rs->negeri)->nama }}</div>
                     </div>
                 </div>
 
                 <div class="form-group">
                     <div class="row">
-                        <label class="col-sm-3 control-label"><b>Nama Pembekal : </b></label>
+                        <label class="col-sm-4 control-label"><b>Nama Pembekal : </b></label>
                         <div class="col-sm-5">{{$rs->nama_pembekal}}</div>
                     </div>
                 </div>
                 
                 <div class="form-group">
                     <div class="row">
-                        <label class="col-sm-3 control-label"><b>Alamat Pembekal : </b></label>
+                        <label class="col-sm-4 control-label"><b>Alamat Pembekal : </b></label>
                         <div class="col-sm-5">{{$rs->alamat_pembekal_1}} {{$rs->alamat_pembekal_2}} {{$rs->alamat_pembekal_3}} {{$rs->poskod_pembekal}}</div>
                     </div>
                 </div>
 
                 <div class="form-group">
                     <div class="row">
-                        <label class="col-sm-3 control-label"><b>Dokumentasi Ramuan : </b></label>
+                        <label class="col-sm-4 control-label"><b>Dokumentasi Ramuan : </b></label>
                         <div class="row">
                             <div class="col-sm-5">Sijil Halal: <a href="">sijilhalal.pdf</a></div>
                         </div>
@@ -118,7 +138,7 @@ if((!empty($id)) && ($upload != '')){
                 @if($rs->is_delete == '1')
                 <div class="form-group">
                     <div class="row">
-                        <label class="col-sm-3 control-label"><b>Sebab Hapus : </b></label>
+                        <label class="col-sm-4 control-label"><b>Sebab Hapus : </b></label>
                         <div class="col-sm-5">{{ strip_tags($rs->delete_comment) }}</div>
                     </div>
                 </div>
